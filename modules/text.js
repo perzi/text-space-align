859ea070808ee56dff8bebe09d1041ce721cfd99
Per Zimmerman <per.zimmerman@gmail.com>
2015-10-09 10:43:33 +0200

    Columnify and decolumnify with tests

--- modules/text.js
+++ modules/text.js
@@ -1,6 +1,54 @@
-
 exports.text = {
+
+  // TODO: create this depending on language
+  reg: /(?:^\s+){0,1}(?:"[^"\\\r\n]*(?:\\.[^"\\\r\n]*)*"|'[^''\\\r\n]*(?:\\.[^'\\\r\n]*)*'|\/\*.*?\*\/|\/\/.*$|\S+)/g,
+
   parse: function(s) {
-    return s + "-";
+    return s.match(this.reg);
+  },
+
+  columnify: function(s) {
+    var lines = s.split("\n");
+    var tokenizedLines = lines.map(function(line) {
+      return this.parse(line);
+    }, this);
+
+    var maxLengths = [];
+
+    tokenizedLines.forEach(function(tokenizedLine) {
+      var lengths = tokenizedLine.map(function(token) {
+        return token.length;
+      });
+
+      lengths.forEach(function(length, index) {
+        if (maxLengths[index] === undefined || maxLengths[index] < length) {
+          maxLengths[index] = length;
+        }
+      });
+    }, this);
+
+    var result = tokenizedLines.map(function(tokenizedLine) {
+      var paddedTokens = tokenizedLine.map(function(token, index) {
+        var pad = maxLengths[index] - token.length + 1;
+        return token + (new Array(pad)).join(" ");
+      });
+
+      return paddedTokens.join(" ");
+    });
+
+    var output = result.join("\n");
+
+    return output;
+  },
+
+  decolumnify: function(s) {
+    var lines = s.split("\n");
+
+    var result = lines.map(function(line) {
+      return this.columnify(line);
+    }, this);
+
+    var output = result.join("\n");
+    return output;
   }
 }
--- test/text-spec.js
+++ test/text-spec.js
@@ -2,9 +2,25 @@ var assert = require("assert");
 var text = require("../modules/text").text;
 
 describe('text', function() {
-  describe('#parse()', function () {
-    it('add dash to text', function () {
-      assert.equal(text.parse("x"), "x-");
+  describe('#columnify()', function () {
+
+    it('should fix spaces 1', function () {
+      assert.equal(text.columnify("let x = 3"), "let x = 3");
+      assert.equal(text.columnify("let x  = 3"), "let x = 3");
+    });
+
+    it('should fix lines', function () {
+      assert.equal(text.columnify("let x = 3;\nlet ab = 4;"), "let x  = 3;\nlet ab = 4;");
+    });
+
+    it('should preserve initial indentation', function () {
+      assert.equal(text.columnify("  let x = 3;\nlet ab = 4;"), "  let x  = 3;\nlet   ab = 4;");
+    });
+  });
+
+  describe('#decolumnify()', function () {
+    it('remove any column alignment', function () {
+      assert.equal(text.decolumnify("let x     = 3;\nlet ab     = 4;"), "let x = 3;\nlet ab = 4;");
     });
   });
 });
